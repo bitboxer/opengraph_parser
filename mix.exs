29cda144b4183939e1b45e87d55b2da780ce3fe5
Bodo Tasche <bodo@wannawork.de>
2025-02-20 22:03:13 +0100

    release 0.7.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpenGraph.Mixfile do
   def project do
     [
       app: :opengraph_parser,
-      version: "0.6.0",
+      version: "0.7.0",
       elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
